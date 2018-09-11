af0c6907f3a993fe978ba3f445cff6cb48c771e6
techdeus <sullivan.marlon@gmail.com>
2018-09-10 22:17:34 -0700

    stringify recursion function complete

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,7 +2,30 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
+// A method on the browser to turn any object into a string representation.
+// opposite of JSON parse();
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+ if (Array.isArray(obj)) { // is the obj an array ?
+ 	var results = []; // declare an empty array
+ 	for (var i = 0; i < obj.length; i++) { // loop through the array
+ 		results.push(stringifyJSON(obj[i])); // push the array value through the stringify method to pass a stringified version to the results array
+ 	}
+ 	return '['+results.join(',')+']'; // join all values separated by comma and concatenate string quotes around the array to convert to a string
+ }
+ if (obj && typeof obj === 'object') {
+ 	var results = [];
+    
+    for (var key in obj) {
+    	if ( obj[key] === undefined || typeof(obj[key]) === 'function' ) {
+    		continue;
+    	}
+    	results.push(stringifyJSON(key)+':'+stringifyJSON(obj[key]) );
+    }
+ 	return '{'+results.join(',')+'}';
+ }
+ if ( typeof obj === 'string' ) { // if obj is a string concatenate it with string literals
+ 	return '"'+obj+'"';
+ }
+ return ''+obj; // if a number, null or boolean, use type coercion to convert to a string
 };
